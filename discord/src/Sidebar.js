d3745a6a3161cdf3a5095b558affebb48833371b
Richa Ulivia Hilda Srianty Purba <richathorne@gmail.com>
2021-02-03 22:29:14 +0000

    Install material-ui packages and style the upper sidebar

--- discord/src/Sidebar.js
+++ discord/src/Sidebar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Sidebar.css";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 function Sidebar() {
 	return (
 		<div className="sidebar">
-			<h2>Sidebar stuff</h2>
 			<div className="sidebar__top">
 				<h3>Built by Richa</h3>
+				<ExpandMoreIcon />
 			</div>
 		</div>
 	);
