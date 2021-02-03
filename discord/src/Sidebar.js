f182211e520fa6a9eef5cbd3d8f6f97e9f0079f9
Richa Ulivia Hilda Srianty Purba <richathorne@gmail.com>
2021-02-03 22:55:07 +0000

    Add the sidebar header and style it

--- discord/src/Sidebar.js
+++ discord/src/Sidebar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Sidebar.css";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
 
 function Sidebar() {
 	return (
@@ -8,6 +9,15 @@ function Sidebar() {
 			<div className="sidebar__top">
 				<h3>Built by Richa</h3>
 				<ExpandMoreIcon />
+			</div>
+			<div className="sidebar__channels">
+				<div className="channels__header">
+					<div className="sidebar__header">
+						<ExpandMoreIcon />
+						<h4>Text Channels</h4>
+					</div>
+					<AddIcon className="sidebar__addChannel" />
+				</div>
 			</div>
 		</div>
 	);
