96440e9da6f8415b8f6fec2f3b6d7b5621c33d86
Richa Ulivia Hilda Srianty Purba <richathorne@gmail.com>
2021-02-05 10:48:51 +0000

    Done with the Google authentication

--- discord/src/Login.js
+++ discord/src/Login.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Login.css";
 import { Button } from "@material-ui/core";
-import { auth } from "./firebase";
+import { auth, provider } from "./firebase";
 
 function Login() {
 	const signIn = () => {
-		auth;
-	}
+		auth.signInWithPopup(provider).catch((error) => alert(error.message));
+	};
 	return (
 		<div className="login">
 			<div className="login__logo">
