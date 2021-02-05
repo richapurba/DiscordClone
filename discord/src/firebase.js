7903bccf917b21e8eaafd6cda350a1b0b2a11144
Richa Ulivia Hilda Srianty Purba <richathorne@gmail.com>
2021-02-05 15:08:02 +0000

    Fix the typo

--- discord/src/firebase.js
+++ discord/src/firebase.js
@@ -1,8 +1,10 @@
 import firebase from "firebase";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB7JMsCAQsD786wdolrmuQyc7IQIg8FEUo",
   authDomain: "discord-clone-22c0f.firebaseapp.com",
+  databaseURL: "https://discord-clone-22c0f-default-rtdb.firebaseio.com",
   projectId: "discord-clone-22c0f",
   storageBucket: "discord-clone-22c0f.appspot.com",
   messagingSenderId: "1060287654205",
@@ -10,7 +12,7 @@ const firebaseConfig = {
   measurementId: "G-5VEL788YGN"
 };
 
-const firebaseApp = firebase.initialzeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
