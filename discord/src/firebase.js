6cc3f53c30466a9c5b06fbd7478f60abca6154ca
Richa Ulivia Hilda Srianty Purba <richathorne@gmail.com>
2021-02-05 08:53:44 +0000

    Add firebase config

---
+++ discord/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB7JMsCAQsD786wdolrmuQyc7IQIg8FEUo",
+  authDomain: "discord-clone-22c0f.firebaseapp.com",
+  projectId: "discord-clone-22c0f",
+  storageBucket: "discord-clone-22c0f.appspot.com",
+  messagingSenderId: "1060287654205",
+  appId: "1:1060287654205:web:8631888ed414cecdd2fd54",
+  measurementId: "G-5VEL788YGN"
+};
