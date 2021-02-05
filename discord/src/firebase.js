5bc63e40d42a8b970a10402bd2ae852fac4477bb
Richa Ulivia Hilda Srianty Purba <richathorne@gmail.com>
2021-02-05 09:43:15 +0000

    firebase configuration

--- discord/src/firebase.js
+++ discord/src/firebase.js
@@ -1,4 +1,5 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyB7JMsCAQsD786wdolrmuQyc7IQIg8FEUo",
   authDomain: "discord-clone-22c0f.firebaseapp.com",
@@ -8,3 +9,5 @@ const firebaseConfig = {
   appId: "1:1060287654205:web:8631888ed414cecdd2fd54",
   measurementId: "G-5VEL788YGN"
 };
+
+const firebaseApp = firebase.initialzeApp(firebaseConfig); 
