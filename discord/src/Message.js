c266248e50b434055c5f41fa1ba5f334d61582b9
Richa Ulivia Hilda Srianty Purba <richathorne@gmail.com>
2021-02-05 12:31:41 +0000

    Finish the whole app

--- discord/src/Message.js
+++ discord/src/Message.js
@@ -2,13 +2,17 @@ import React from "react";
 import "./Message.css";
 import { Avatar } from "@material-ui/core";
 
-function Message() {
+function Message({ timestamp, user, message }) {
 	return (
 		<div className="message">
-			<Avatar />
+			<Avatar src={user.photo} />
 			<div className="message__info">
-				<h4>richaulivia <span className="message__timestamp">timestamp</span></h4>
-				<p>The message</p>
+				<h4>{user.displayName} 
+					<span className="message__timestamp">
+						{new Date(timestamp?.toDate()).toUTCString()}
+					</span>
+				</h4>
+				<p>{message}</p>
 			</div>
 		</div>
 	);
